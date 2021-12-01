e764d172e6e295b0e707570727ab0c8e4b6543c6
UWAJENEZANadine <adelineu640@gmail.com>
2021-12-01 12:03:11 +0200

    additional changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from "../assets/2.PNG";
 import "./header.css";
+import { Modal, Form, Input, Button } from 'antd';
+import {useNavigate} from "react-router-dom"
 const Header = () => {
+    const [visible, setVisible]= useState(false);
+    const onFinish=(values)=>{
+        console.log(values)
+    };
+    const navigate= useNavigate();
     return (
+        <>
+        <Modal visible={visible}
+         width="40%"
+         onOk={()=>setVisible(true)}
+         onCancel={() =>setVisible(false)}
+         >
+            <h1>Signin Form</h1>
+            <Form onFinish={onFinish}>
+                <Form.Item label="Email" name="email" rules={[{required:true, type:"email"}]}>
+                    <Input type="email"/>
+
+                </Form.Item>
+                <Form.Item label="Password" name="password" rules={[{required:true}]}>
+                    <Input type="password"/>
+
+                </Form.Item>
+                <Button htmlType="submit" onClick={()=>{
+                    localStorage.setItem("userLogedIn", true);
+                    navigate("/dash/NewTour")
+
+                }}>Login</Button>
+            </Form>
+
+        </Modal>
         <div className="container-logo">
             <img src={logo} />
             <div className="nav-bar">
@@ -11,11 +42,13 @@ const Header = () => {
                 <a href="/tours">Tours</a>
                 <a href="/Aboutus">About us </a>
                 <a href="/home">Home </a>
+                <a href="#" onClick={()=>setVisible(true)}>signIn</a>
                
 
             </div>
 
         </div>
+        </>
 
     )
 }
